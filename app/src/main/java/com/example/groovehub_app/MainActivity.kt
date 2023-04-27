113c8501931e7815cf87a8066ffc39519e5ec430
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2023-04-27 01:22:15 -0500

    feat: binding attribute added

--- app/src/main/java/com/example/groovehub_app/MainActivity.kt
+++ app/src/main/java/com/example/groovehub_app/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.groovehub_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.groovehub_app.adapter.NotificationAdapter
+import com.example.groovehub_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NotificationAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // inflate the layout
+        setContentView(binding.root) // set the content view
+
+        adapter = NotificationAdapter()
+        //TODO: Create notifications fragment with the recycler view
+        //binding.recyclerView.adapter = adapter
     }
 }
