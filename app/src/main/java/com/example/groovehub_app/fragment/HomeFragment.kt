813c7158d474605a0e6ba581fa2a869e6e37d09b
Paula Andrea Trujillo <71205932+PaulaTrujillo27@users.noreply.github.com>
2023-05-01 18:21:51 -0500

    feat:fragments creation

---
+++ app/src/main/java/com/example/groovehub_app/fragment/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.groovehub_app.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.groovehub_app.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
