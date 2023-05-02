e1f80fbf8640bac2753c6bbeaa1ea9138a9d7b2a
camilogonzalez7424 <gonzalezcamilo508@gmail.com>
2023-05-01 21:05:35 -0500

    feat:conection with Firebase

--- app/src/main/java/com/example/groovehub_app/RegisterActivity.kt
+++ app/src/main/java/com/example/groovehub_app/RegisterActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.OnBackPressedCallback
 import com.example.groovehub_app.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -15,6 +18,8 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonRegister.setOnClickListener(::register)
 
         val intent = Intent(this,LoginActivity::class.java)
 
@@ -29,6 +34,14 @@ class RegisterActivity : AppCompatActivity() {
 
         onBackPressedDispatcher.addCallback(this,callback)
 
+    }
+
+    fun register(view: View){
+        Firebase.auth.createUserWithEmailAndPassword(
+            binding.editTextEmail?.text.toString(),
+            binding.editTextPassword?.text.toString()
+
+        )
     }
 
 
