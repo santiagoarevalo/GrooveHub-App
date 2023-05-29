230ac0647ab9e115a9dc7e4a8c497ebfaca44bee
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2023-05-28 23:49:02 -0500

    feat: publish a post implemented (temp)

--- app/src/main/java/com/example/groovehub_app/NewPostActivity.kt
+++ app/src/main/java/com/example/groovehub_app/NewPostActivity.kt
@@ -1,9 +1,18 @@
 package com.example.groovehub_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.groovehub_app.databinding.ActivityLoginBinding
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.groovehub_app.databinding.ActivityNewPostBinding
+import com.example.groovehub_app.model.Post
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class NewPostActivity : AppCompatActivity() {
 
@@ -13,5 +22,36 @@ class NewPostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNewPostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.publishBttn.setOnClickListener(::publishPost)
+    }
+
+    private fun publishPost(view: View) {
+        val title = binding.titleEditTxt.text.toString()
+        val description = binding.descriptionEditTextMulti.text.toString()
+        val username = FirebaseAuth.getInstance().currentUser!!.email.toString()
+        val post = Post(0,title,username, description, 0)
+        val postId = UUID.randomUUID().toString()
+
+        if(!verifyPostFields(title, description)) {
+            println(verifyPostFields(title, description))
+            Firebase.firestore.collection("posts").document(postId).set(post);
+            Toast.makeText(this, "¡Publicación exitosa!", Toast.LENGTH_SHORT).show();
+            returnMainActivity()
+        }else {
+            Toast.makeText(this, "Llena los campos de la publicación", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun verifyPostFields(title: String, description: String) : Boolean {
+        return (title.isEmpty() || description.isEmpty())
+    }
+
+    private fun returnMainActivity() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
